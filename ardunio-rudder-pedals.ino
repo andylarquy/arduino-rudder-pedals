b4bd4ad2f60ff2f6841b157b3291ec93e301f1c9
Andres Suarez <andres27059934@gmail.com>
2023-07-09 22:56:27 -0300

    refac: implement two axis for individual pedal braking

--- ardunio-rudder-pedals.ino
+++ ardunio-rudder-pedals.ino
@@ -1,11 +1,12 @@
 #include <Joystick.h>
 
-Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_MULTI_AXIS, 0, 0, false, false, false, false, false, false, true, false, false, true, false);
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_MULTI_AXIS, 0, 0, true, true, false, false, false, false, true, false, false, false, false);
 
 int rightPedalPin = A1;
 int leftPedalPin = A0;
 int CONSTANT_5V = 4;
-int MAX_RUDDER_RANGE = 1024;
+int MIDDLE_RUDDER_RANGE = 512;
+int DEAD_ZONE_BRAKING_DETECTION = 100;
 
 void setup() {
   // Debug
@@ -21,26 +22,30 @@ void loop() {
   //Output 5V
   digitalWrite(CONSTANT_5V, HIGH);
 
-  int leftPedal = min(analogRead(leftPedalPin) * 1.2, MAX_RUDDER_RANGE / 2);
-  int rightPedal = min(analogRead(rightPedalPin) * 1.55, MAX_RUDDER_RANGE / 2);
+  int leftPedal = min(analogRead(leftPedalPin) * 2, MIDDLE_RUDDER_RANGE);
+  int rightPedal = min(analogRead(rightPedalPin) * 1.55, MIDDLE_RUDDER_RANGE);
   int brakePedal = min(leftPedal, rightPedal) * 2;
+  int finalRudderPosition;
+  if (leftPedal >= DEAD_ZONE_BRAKING_DETECTION && rightPedal >= DEAD_ZONE_BRAKING_DETECTION) {
+    finalRudderPosition = MIDDLE_RUDDER_RANGE;
+  } else {
+    finalRudderPosition = MIDDLE_RUDDER_RANGE + rightPedal - leftPedal;
+  }
 
-  int neutralRudderPosition = MAX_RUDDER_RANGE / 2;
-  int finalRudderPosition = neutralRudderPosition + rightPedal - leftPedal;
-
-// Debug
+  // Debug
   Serial.print("Final rudder position: ");
   Serial.println(finalRudderPosition);
 
   Joystick.setRudder(finalRudderPosition);
-  Joystick.setBrake(brakePedal);
+  Joystick.setXAxis(brakePedal);
+  Joystick.setYAxis(brakePedal);
 
   Joystick.sendState();
- 
+
   // Debug
   Serial.print("Left pedal: ");
   Serial.println(leftPedal);
-  
+
   Serial.print("Right pedal: ");
   Serial.println(rightPedal);
 
